4501c56db63567796d6918a6c1a0667caf45038a
Athosgpm <athos.gpm@gmail.com>
2021-01-28 15:39:54 -0300

    exercicio03

--- java/aulas/src/exercicio03/Q06SomaDiv3.java
+++ java/aulas/src/exercicio03/Q06SomaDiv3.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Q06SomaDiv3 {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int a,soma=0;
+		int a,soma=0,cont=0;
 		
 		do {
 			System.out.println("Digite o valor do numero: ");
 			a=ler.nextInt();
 			if(a%3==0) {
 				soma+=a;
+				cont++;
 			}
 		}while(a!=0);
 		
-		System.out.printf("O valor da soma dos numeros é: %d",soma);
+		System.out.printf("O valor da media dos numeros multiplos de 3 é: %d",(1.0*soma/cont));
 	}
 }
